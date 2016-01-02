9ab190a3c159d4a372814986b0a5c70213144052
Alan R. Soares <alanrsoars@gmail.com>
2016-01-02 18:22:46 +1300

    two joggers

---
+++ two_joggers/two_joggers.js
@@ -0,0 +1,4 @@
+const gcd = (a, b) => !b ? a : gcd(b, a % b)
+const lcm = (a, b) => (a * b) / gcd(a,b)
+const laps = (lcm, x, y) => [lcm / x, lcm / y]
+const nbrOfLaps = (x, y) => laps(lcm(x, y), x, y)
